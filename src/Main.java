553856d869c8e12e6034fe6c2a07e413ef82e0b7
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-12-06 22:30:50 +0300

    revision 3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Book book = new Book ("A Bit of Darkness ", "Lyubko Deresh " , 2007);
         Author author1 = new Author ("Virginia ", "Wolf");
         Book book1 = new Book ("White Fang ", "Virginia Woolf ", 1906);
-        book1.setPublishingDate(1908);
+        book1.setPublishingDate(1907);
         System.out.println(book.getBookName() + author.getName() + author.getSurname() + " " + book.getPublishingDate());
         System.out.println(book1.getPublishingDate());
         System.out.println(book1.getBookName() + author1.getName() + author1.getSurname() + " " + book1.getPublishingDate());
